a24cb94cab2dbdb685b9238055d16c7beec1e0a2
Jarkko Vatjus-Anttila <jvatjusanttila@gmail.com>
2011-07-27 22:24:23 +0300

    Update to core generator to match TXML and Terrain class APIs

--- WorldGenerator.py
+++ WorldGenerator.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
-# WorldGenerator.py <width> <height>
-# defaults:
-#  - width  10
-#  - height 10
+#
+# Author: Jarkko Vatjus-Anttila <jvatjusanttila@gmail.com>
+#
+# For conditions of distribution and use, see copyright notice in license.txt
+#
 
+import sys
 import random
-from TerrainGenerator import *
+
+import TerrainGenerator
+import TXMLOutput
 
 class WorldGenerator():
     """ class WorldGenerator():
@@ -14,104 +18,52 @@ class WorldGenerator():
     """
 
     def __init__(self):
-        self.indent = 0
-        self.id = 1
         self.cPatchSize = 16
         self.cPatchSize = 16
-        self.terrain = TerrainGenerator()
+        self.terrain = TerrainGenerator.TerrainGenerator()
+        self.TXML = TXMLOutput.TXMLOutput()
 
-    ### Output methods, for writing txml ###
-
-    def output(self, string):
-        """ WorldGenerator.output(string):
-            - Takes a null terminated string as input and writes it into the output stream.
-              the current implementation writes directly to stdout, and the purpose is to
-              allow redirection of the output in shell to what ever target.
-            - It is assumed that the input string is already formatted for output.
-            - This method also does automatic intention if the txml output, which is
-              controlled via self.increase_indent() and self.decrease_indent() methods.
-            Return value: None
-        """
-        self.indent_str = ""
-        for i in range(self.indent):
-            self.indent_str = self.indent_str + " "
-        sys.stdout.write(self.indent_str + str(string) + "\n")
-
-    def increase_indent(self):
-        self.indent = self.indent + 1
-
-    def decrease_indent(self):
-        self.indent = self.indent - 1
-        if self.indent < 0:
-            self.indent = 0
+    ### Error logging to stderr ###
 
     def message(self, string):
         sys.stderr.write(string + "\n")
 
     ### Attribute creation, component and entity start and stop signals
 
-    def start_scene(self):
-        self.output("<!DOCTYPE Scene>\n<scene>")
-        self.increase_indent()
-
-    def end_scene(self):
-        self.decrease_indent()
-        self.output("</scene>")
-
-    def start_component(self, component, sync):
-        self.output("<component type=\"" + str(component) + "\" sync=\"" + str(sync) + "\">")
-        self.increase_indent()
-
-    def end_component(self):
-        self.decrease_indent()
-        self.output("</component>")
-
-    def start_entity(self):
-        self.output("<entity id=\"" + str(self.id) + "\">")
-        self.increase_indent()
-
-    def end_entity(self):
-        self.decrease_indent()
-        self.output("</entity>")
-        self.id = self.id + 1
-
-    def create_attribute(self, name, value):
-        self.output("<attribute value=\"" + str(value) + "\" name=\"" + str(name) + "\"/>")
-
     def create_component_rigidbody(self, reference, mass, shapetype, collision):
-        self.start_component("EC_RigidBody", "1")
-        self.create_attribute("Mass", str(mass))
-        self.create_attribute("Shape type", str(shapetype))
+        self.TXML.startComponent("EC_RigidBody", "1")
+        self.TXML.createAttribute("Mass", str(mass))
+        self.TXML.createAttribute("Shape type", str(shapetype))
         if collision != "": c_str = str(reference) + str(collision)
         else: c_str = ""
-        self.create_attribute("Collision mesh ref", c_str)
-        self.create_attribute("Friction", "0.5")
-        self.create_attribute("Restitution", "0")
-        self.create_attribute("Linear damping", "0")
-        self.create_attribute("Angular damping", "0")
-        self.create_attribute("Linear factor", "1 1 1")
-        self.create_attribute("Angular factor", "1 1 1")
-        self.create_attribute("Phantom", "false")
-        self.create_attribute("Draw Debug", "false")
-        self.create_attribute("Linear velocity", "0 0 0")
-        self.create_attribute("Angular velocity", "0 0 0")
-        self.end_component()
+        self.TXML.createAttribute("Collision mesh ref", c_str)
+        self.TXML.createAttribute("Friction", "0.5")
+        self.TXML.createAttribute("Restitution", "0")
+        self.TXML.createAttribute("Linear damping", "0")
+        self.TXML.createAttribute("Angular damping", "0")
+        self.TXML.createAttribute("Linear factor", "1 1 1")
+        self.TXML.createAttribute("Angular factor", "1 1 1")
+        self.TXML.createAttribute("Phantom", "false")
+        self.TXML.createAttribute("Draw Debug", "false")
+        self.TXML.createAttribute("Linear velocity", "0 0 0")
+        self.TXML.createAttribute("Angular velocity", "0 0 0")
+        self.TXML.endComponent()
 
     def create_component_name(self, name, description, userdefined):
-        self.start_component("EC_Name", "1")
-        self.create_attribute("name", str(name))
-        self.create_attribute("description", str(description))
-        self.create_attribute("user-defined", str(userdefined))
-        self.end_component()
+        self.TXML.startComponent("EC_Name", "1")
+        self.TXML.createAttribute("name", str(name))
+        self.TXML.createAttribute("description", str(description))
+        self.TXML.createAttribute("user-defined", str(userdefined))
+        self.TXML.endComponent()
 
     def create_component_placeable(self, transform):
-        self.start_component("EC_Placeable", "1")
-        self.create_attribute("Position", "0 0 0")
-        self.create_attribute("Scale", "0 0 0")
-        self.create_attribute("Transform", str(transform))
-        self.create_attribute("Show bounding box", "false")
-        self.create_attribute("Visible", "true")
-        self.end_component()
+        self.TXML.startComponent("EC_Placeable", "1")
+        self.TXML.createAttribute("Position", "0 0 0")
+        self.TXML.createAttribute("Scale", "0 0 0")
+        self.TXML.createAttribute("Transform", str(transform))
+        self.TXML.createAttribute("Show bounding box", "false")
+        self.TXML.createAttribute("Visible", "true")
+        self.TXML.endComponent()
 
     ### Logical components for the world
 
@@ -124,95 +76,95 @@ class WorldGenerator():
         return m_str
 
     def create_static_mesh(self, name, mesh, reference, materials, center_x, center_y, center_z=0.0):
-        self.start_entity()
+        self.TXML.startEntity()
         # Mesh component
-        self.start_component("EC_Mesh", "1")
-        self.create_attribute("Transform", "0,0,0,90,0,180,1,1,1")
-        self.create_attribute("Mesh ref", str(reference) + str(mesh))
-        self.create_attribute("Skeleton ref", "")
-        self.create_attribute("Mesh materials", self.insert_materials(reference, materials))
-        self.create_attribute("Draw distance", "0")
-        self.create_attribute("Cast shadows", "true")
-        self.end_component()
+        self.TXML.startComponent("EC_Mesh", "1")
+        self.TXML.createAttribute("Transform", "0,0,0,90,0,180,1,1,1")
+        self.TXML.createAttribute("Mesh ref", str(reference) + str(mesh))
+        self.TXML.createAttribute("Skeleton ref", "")
+        self.TXML.createAttribute("Mesh materials", self.insert_materials(reference, materials))
+        self.TXML.createAttribute("Draw distance", "0")
+        self.TXML.createAttribute("Cast shadows", "true")
+        self.TXML.endComponent()
         # Name -component
         self.create_component_name(str(name), "", "false")
         # Placeable -component
-        self.start_component("EC_Placeable", "1")
-        self.create_attribute("Position", "0 0 0")
-        self.create_attribute("Scale", "0 0 0")
-        self.create_attribute("Transform", str(center_x)+","+str(center_y)+","+str(center_z)+",0,0,180,1,1,1")
-        self.create_attribute("Show bounding box", "false")
-        self.create_attribute("Visible", "true")
-        self.end_component()
+        self.TXML.startComponent("EC_Placeable", "1")
+        self.TXML.createAttribute("Position", "0 0 0")
+        self.TXML.createAttribute("Scale", "0 0 0")
+        self.TXML.createAttribute("Transform", str(center_x)+","+str(center_y)+","+str(center_z)+",0,0,180,1,1,1")
+        self.TXML.createAttribute("Show bounding box", "false")
+        self.TXML.createAttribute("Visible", "true")
+        self.TXML.endComponent()
         # Rigidbody -component
         self.create_component_rigidbody(reference, "1", "4", mesh)
-        self.end_entity()
+        self.TXML.endEntity()
 
     def create_terrain(self, reference, materials, heightmap, collision, width, height):
-        self.start_entity()
-        self.create_component_name("terrain"+str(self.id), "", "false")
-        self.start_component("EC_Terrain", "1")
-        self.create_attribute("Transform", "-"+str(width*8)+",-"+str(height*8)+",0,0,0,0,1,1,1")
-        self.create_attribute("Grid Width", str(width))
-        self.create_attribute("Grid Height", str(height))
-        self.create_attribute("Tex. U scale", "0.129999995")
-        self.create_attribute("Tex. V scale", "0.129999995")
-        self.create_attribute("Material", self.insert_materials(reference, materials))
-        self.create_attribute("Texture 0", "")
-        self.create_attribute("Texture 1", "")
-        self.create_attribute("Texture 2", "")
-        self.create_attribute("Texture 3", "")
-        self.create_attribute("Texture 4", "")
-        self.create_attribute("Heightmap", str(reference) + str(heightmap))
-        self.end_component()
+        self.TXML.startEntity()
+        self.create_component_name("terrain"+str(self.TXML.getCurrentEntityID()), "", "false")
+        self.TXML.startComponent("EC_Terrain", "1")
+        self.TXML.createAttribute("Transform", "-"+str(width*8)+",-"+str(height*8)+",0,0,0,0,1,1,1")
+        self.TXML.createAttribute("Grid Width", str(width))
+        self.TXML.createAttribute("Grid Height", str(height))
+        self.TXML.createAttribute("Tex. U scale", "0.129999995")
+        self.TXML.createAttribute("Tex. V scale", "0.129999995")
+        self.TXML.createAttribute("Material", self.insert_materials(reference, materials))
+        self.TXML.createAttribute("Texture 0", "")
+        self.TXML.createAttribute("Texture 1", "")
+        self.TXML.createAttribute("Texture 2", "")
+        self.TXML.createAttribute("Texture 3", "")
+        self.TXML.createAttribute("Texture 4", "")
+        self.TXML.createAttribute("Heightmap", str(reference) + str(heightmap))
+        self.TXML.endComponent()
         self.create_component_rigidbody(reference, "0", "5", collision)
-        self.end_entity()
+        self.TXML.endEntity()
 
     def create_avatar(self, reference, script):
-        self.start_entity()
-        self.start_component("EC_Script", "1")
-        self.create_attribute("Type", "js")
-        self.create_attribute("Run on load", "true")
-        self.create_attribute("Script ref", str(reference) + str(script))
-        self.end_component()
-        self.create_component_name("AvatarScript"+str(self.id), "", "false")
-        self.end_entity()
+        self.TXML.startEntity()
+        self.TXML.startComponent("EC_Script", "1")
+        self.TXML.createAttribute("Type", "js")
+        self.TXML.createAttribute("Run on load", "true")
+        self.TXML.createAttribute("Script ref", str(reference) + str(script))
+        self.TXML.endComponent()
+        self.create_component_name("AvatarScript"+str(self.TXML.getCurrentEntityID()), "", "false")
+        self.TXML.endEntity()
 
     def create_waterplane(self, width, height, level):
-        self.start_entity()
-        self.create_component_name("WaterPlane"+str(self.id), "", "false")
+        self.TXML.startEntity()
+        self.create_component_name("WaterPlane"+str(self.TXML.getCurrentEntityID()), "", "false")
         self.create_component_placeable("0,0,"+str(level)+",0,0,0,1,1,1")
-        self.start_component("EC_WaterPlane", "1")
-        self.create_attribute("x-size", str(width))
-        self.create_attribute("y-size", str(height))
-        self.create_attribute("depth", "20")
-        self.create_attribute("Position", "0 0 0")
-        self.create_attribute("Rotation", "1 0 0 0")
-        self.create_attribute("U factor", "0.00199999995")
-        self.create_attribute("V factor", "0.00199999995")
-        self.create_attribute("Segments in x", "10")
-        self.create_attribute("Segments in y", "10")
-        self.create_attribute("Material", "Ocean")
-        self.create_attribute("Material ref", "")
-        self.create_attribute("Fog color", "0.2 0.4, 0.35")
-        self.create_attribute("Fog start dist.", "100")
-        self.create_attribute("Fog end dist.", "2000")
-        self.create_attribute("Fog mode", "3")
-        self.end_component()
-        self.end_entity()
+        self.TXML.startComponent("EC_WaterPlane", "1")
+        self.TXML.createAttribute("x-size", str(width))
+        self.TXML.createAttribute("y-size", str(height))
+        self.TXML.createAttribute("depth", "20")
+        self.TXML.createAttribute("Position", "0 0 0")
+        self.TXML.createAttribute("Rotation", "1 0 0 0")
+        self.TXML.createAttribute("U factor", "0.00199999995")
+        self.TXML.createAttribute("V factor", "0.00199999995")
+        self.TXML.createAttribute("Segments in x", "10")
+        self.TXML.createAttribute("Segments in y", "10")
+        self.TXML.createAttribute("Material", "Ocean")
+        self.TXML.createAttribute("Material ref", "")
+        self.TXML.createAttribute("Fog color", "0.2 0.4, 0.35")
+        self.TXML.createAttribute("Fog start dist.", "100")
+        self.TXML.createAttribute("Fog end dist.", "2000")
+        self.TXML.createAttribute("Fog mode", "3")
+        self.TXML.endComponent()
+        self.TXML.endEntity()
 
     ### High level world generation itself
 
     def generateworld(self, width, height):
-        self.start_scene()
+        self.TXML.startScene()
 
         self.message("Generating terrain...")
         self.create_terrain("local://", ("terrainweighted2.material",), "terrain.ntf", "", width, height)
         self.terrain.fromdiamondsquare(width, 10, -5, -5, 10)
-        self.terrain.apply_rescale(-20, 50)
-        self.terrain.apply_saturate(-5)
-        self.terrain.create_weightmap("./blender-export/terrain.tga")
-        self.terrain.tofile("./blender-export/terrain.ntf")
+        self.terrain.rescale(-20, 50)
+        self.terrain.saturate(-5)
+        self.terrain.create_weightmap("./resources/terrain.tga")
+        self.terrain.tofile("./resources/terrain.ntf")
 
         self.create_waterplane(width*self.cPatchSize, height*self.cPatchSize, -1)
         self.create_avatar("local://", "avatarapplication.js")
@@ -225,17 +177,20 @@ class WorldGenerator():
             x = x - width*self.cPatchSize/2
             y = y - height*self.cPatchSize/2
             if (z > 2.0) and (z < self.terrain.get_maxitem()/2.0):
-                self.create_static_mesh("Tree"+str(self.id),
+                self.create_static_mesh("Tree"+str(self.TXML.getCurrentEntityID()),
                                         "sassa_asimina_g.mesh",
                                         "local://",
                                         ("m_sassafaras_leaves.material", "m_asimina_bark.material", "m_asimina_leaves.material"),
                                         y, x, z)
                 #print "creaeting in %d %d %d" % (x, y, z)
 
-        self.end_scene()
+        self.TXML.endScene()
 
 ###
 
 if __name__ == "__main__": # if run standalone
+    # By a default test case we shall run an embedded world generator script to create a simple
+    # world with terrain and a few objects in it. Feel free to rewrite the generator using
+    # world generator primitives, by yourself.
     world = WorldGenerator()
     world.generateworld(32, 32)
