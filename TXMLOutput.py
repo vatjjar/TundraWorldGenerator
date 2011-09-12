5f1f5f3474980f150e30631a59ebffb4e8032f81
Jarkko Vatjus-Anttila <jvatjusanttila@gmail.com>
2011-09-12 11:58:11 +0300

    TXMLOutput() now uses internal buffer insteaf of stdout output

--- TXMLOutput.py
+++ TXMLOutput.py
@@ -5,7 +5,7 @@
 # For conditions of distribution and use, see copyright notice in license.txt
 #
 
-import sys
+import sys, os
 
 class TXMLOutput():
     """ class TXMLOutput(): I/O component for writing formatted TXML notation
@@ -13,66 +13,100 @@ class TXMLOutput():
     def __init__(self):
         self.indent = 0
         self.entity_id = 0
+        self.resetOutputXML()
 
-    ### Helpers
+#############################################################################
+# TXML output methods
+#
 
-    def output(self, string):
-        self.indent_str = ""
+    def resetOutputXML(self):
+        self.outputXMLbuffer = ""
+
+    def outputXML(self, msg):
+        indent_str = ""
         for i in range(self.indent):
-            self.indent_str = self.indent_str + " "
-        sys.stdout.write(self.indent_str + str(string) + "\n")
+            indent_str = indent_str + " "
+        self.outputXMLbuffer += (indent_str + str(msg) + "\n")
 
-    def increase_indent(self):
-        self.indent = self.indent + 1
+    def toFile(self, filename, overwrite=False):
+        if os.path.exists(filename):
+            if overwrite == False:
+                sys.stderr.write("TXMLOutput: ERROR: output file '%s' already exists!\n" % filename)
+                return
+            else:
+                os.remove(filename)
+        try: file = open(filename, "w")
+        except IOError:
+            sys.stderr.write("TXMLOutput: ERROR: Unable to open file '%s' for writing!" % filename)
+            return
+        file.write(self.outputXMLbuffer)
+        file.close()
 
-    def decrease_indent(self):
-        self.indent = self.indent - 1
-        if self.indent < 0: self.indent = 0
+    def increaseIndent(self):
+        self.indent += 1
 
-    ### Getters/setters
+    def decreaseIndent(self):
+        if self.indent > 0: self.indent -= 1
+
+#############################################################################
+# TXML getters/setters
+#
 
     def getCurrentEntityID(self):
         return self.entity_id
 
-    ### Scene level
+#############################################################################
+# TXML scene level
+#
 
     def startScene(self):
-        self.output("<!DOCTYPE Scene>\n<scene>")
-        self.increase_indent()
+        self.outputXML("<!DOCTYPE Scene>\n<scene>")
+        self.increaseIndent()
 
     def endScene(self):
-        self.decrease_indent()
-        self.output("</scene>")
+        self.decreaseIndent()
+        self.outputXML("</scene>")
 
-    ### Entity level
+#############################################################################
+# TXML entity level
+#
 
     def startEntity(self):
-        self.output("<entity id=\"" + str(self.entity_id) + "\">")
-        self.increase_indent()
+        self.outputXML("<entity id=\"" + str(self.entity_id) + "\">")
+        self.increaseIndent()
 
     def endEntity(self):
-        self.decrease_indent()
-        self.output("</entity>")
+        self.decreaseIndent()
+        self.outputXML("</entity>")
         self.entity_id = self.entity_id + 1
 
-    ### Component level
+#############################################################################
+# TXML component level
+#
 
     def startComponent(self, component, sync):
-        self.output("<component type=\"" + str(component) + "\" sync=\"" + str(sync) + "\">")
-        self.increase_indent()
+        self.outputXML("<component type=\"" + str(component) + "\" sync=\"" + str(sync) + "\">")
+        self.increaseIndent()
 
     def endComponent(self):
-        self.decrease_indent()
-        self.output("</component>")
+        self.decreaseIndent()
+        self.outputXML("</component>")
 
-    ### Attribute level
+#############################################################################
+# TXML attribute level
+#
 
     def createAttribute(self, name, value):
-        self.output("<attribute value=\"" + str(value) + "\" name=\"" + str(name) + "\"/>")
+        self.outputXML("<attribute value=\"" + str(value) + "\" name=\"" + str(name) + "\"/>")
+
+#############################################################################
+# TXML standalone test case
+#
 
 if __name__ == "__main__": # if run standalone
     # The test case here will output a few components to stdout, which will
     # look like TXML output
+    print "Generating test TXML"
     t = TXMLOutput()
     t.startScene()
     t.startEntity()
@@ -86,4 +120,5 @@ if __name__ == "__main__": # if run standalone
     t.endComponent()
     t.endEntity()
     t.endScene()
-
+    print "Writing test output into ./resources/test.txml"
+    t.toFile("./resources/test.txml", overwrite=True)
